f0c8699a7d481d5db2f14e826e3f6c1d7d108d91
gymno <gymnopedien@gmail.com>
2024-05-24 14:43:23 +0900

    Initial SIG commit

---
+++ test.cpp
@@ -0,0 +1,14 @@
+// This file is for github-sig
+
+#include <cstdio>
+
+void MyTest() {
+    int* bbb = new int;
+    *bbb = 555;
+    
+}
+
+int main(int argc, char* argv[]) {
+    printf("hello again\n");
+    return 0;
+}
