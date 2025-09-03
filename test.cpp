a7a40a05bf0766d00739f46829395b9d9ef565d8
bd-karas <140779992+bd-karas@users.noreply.github.com>
2025-09-03 11:01:18 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -14,6 +14,11 @@ void MyTest2() {
     return;    
 }
 
+void MyTest3() {
+    double* aaa = new double;
+    *aaa = 3.14159;
+    return;    
+}
 
 int main(int argc, char* argv[]) {
     printf("hello again\n");
